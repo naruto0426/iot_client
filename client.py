2b655a66363c69e23d49a02fc79547fe9cc25c68
naruto0426 <naruto0426@gmail.com>
2020-05-05 17:47:06 +0800

    .

--- client.py
+++ client.py
@@ -26,7 +26,7 @@ processor = Intel64 Family 6 Model 158 Stepping 10, GenuineIntel
 """
 uid_file_name = 'demo_uid.txt'
 while True:
-    print ("i am in the iot class")
+    print ("ntu iot 112")
     try:
         with open(r'config.yml') as file:
             config = yaml.load(file, Loader=yaml.FullLoader)
